b07f1c835bb596bdc9b924a03d18210b95f87d1c
AO1 <44472390+andrewaimia@users.noreply.github.com>
2019-04-18 13:00:33 +0400

    New Share Summary report

---
+++ ShareSummary/luacontent.lua
@@ -0,0 +1,8 @@
+local total_balance = 0
+function handle_record(record)
+    total_balance = total_balance + record:get('VALUE');
+end
+
+function complete(result)
+    result:set('ASSET_BALANCE', total_balance);
+end
